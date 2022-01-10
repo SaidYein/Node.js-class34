fdde493b2d5e6bcaad32dd931806b36d79d85372
SaidYein <saidyein@gmail.com>
2022-01-10 20:12:30 +0100

    week2 fix HackYourTemperature

--- homework/config-files/hackyourtemperature/__tests__/app.test.js
+++ homework/config-files/hackyourtemperature/__tests__/app.test.js
@@ -5,22 +5,28 @@ const request = supertest(app);
 
 describe("POST /", () => {
   it("if a proper city name is entered, the statusCode must be 200", async () => {
-    const response = await request.post("/weather/london");
+    const response = await request
+      .post("/weather")
+      .send({ cityName: "London" });
     expect(response.statusCode).toBe(200);
   });
 
   it("if a falsy city name is entered, the statusCode must be 400", async () => {
-    const response = await request.post("/weather/xxxx");
+    const response = await request
+      .post("/weather")
+      .send({ cityName: "xxx123" });
     expect(response.statusCode).toBe(400);
   });
 
   it("if no city name is entered, the statusCode must be 404", async () => {
-    const response = await request.post("/weather/");
-    expect(response.statusCode).toBe(404);
+    const response = await request.post("/weather").send({});
+    expect(response.statusCode).toBe(400);
   });
 
   it("Response should include a city name and it's current temperature", async () => {
-    const response = await request.post("/weather/London");
+    const response = await request
+      .post("/weather")
+      .send({ cityName: "London" });
     expect(response.body.weatherText).toContain("London" && ".");
   });
 });
--- homework/config-files/hackyourtemperature/app.js
+++ homework/config-files/hackyourtemperature/app.js
@@ -10,25 +10,21 @@ app.get("/", (req, res) => {
   res.send("hello from backend to frontend!");
 });
 
-app.post("/weather/:cityName", async (req, res) => {
-  const city = req.params.cityName;
+app.post("/weather", async (req, res) => {
+  const city = req.body.cityName;
 
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     );
 
     const data = await response.json();
     if (data.cod === 200) {
       res.json({
-        weatherText: `${data.name} is ${(data.main.temp - 273.15).toFixed(
-          1
-        )} °C degrees`,
+        weatherText: `${data.name} is ${data.main.temp} °C degrees`,
       });
     } else {
       res.status(400).json({
-        //When I return res.statusCode, I got 200. Why I don't get 400 or 404?
-
         weatherText: ` ${res.statusCode}: SIMPLY MEANS, THERE IS SOMETHING WRONG ABOUT THE CITY NAME!`,
       });
     }
